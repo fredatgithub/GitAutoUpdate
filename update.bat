d7d8f2019499fbd578e3f0b15a09b874622b0ea2
Freddy Juhel <domaine75@gmail.com>
2015-07-25 13:58:35 +0200

    initial commit

---
+++ update.bat
@@ -0,0 +1,41 @@
+cd \
+cd "C:\Users\fred\Documents\Visual Studio 2012\Projects"
+cd AddFeatures
+git pull origin master
+cd ..
+cd CodeGeneration
+git pull origin master
+cd ..
+cd PaperBoy
+git pull origin master
+cd ..
+cd fredatgithub.github.io
+git pull origin master
+cd ..
+cd MyFavoriteQuotes
+git pull origin master
+cd ..
+cd WinFormTemplate
+git pull origin master
+cd ..
+cd UsefulFunctions
+git pull origin master
+cd ..
+cd YouTubeVideoName
+git pull origin master
+cd ..
+cd CharacterPermutations
+git pull origin master
+cd ..
+cd MRUManager
+git pull origin master
+cd ..
+cd uptime
+git pull origin master
+cd ..
+cd Matrix
+git pull origin master
+cd ..
+cd Chronometre
+git pull origin master
+cd ..
